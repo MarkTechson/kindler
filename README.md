ef3c88b273a6e859c56cd01c7fc6ef9f111ae3d8
Mark Techson <2554588+MarkTechson@users.noreply.github.com>
2020-01-13 23:40:49 -0600

    Adds step one to the repository

--- README.md
+++ README.md
@@ -1,16 +1,4 @@
-# kindler
+# Kindler App / 1. Getting Started
+`feature/hello_world`
 
-A new Flutter project.
-
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+Head over to `lib/main.dart` to see the code that will render a page that says, "Hello, World"
--- lib/main.dart
+++ lib/main.dart
@@ -18,67 +18,6 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: Text(
-          'Kindler 🔥',
-          style: TextStyle(
-            fontSize: 24.0,
-            color: Colors.red,
-          ),
-        ),
-        centerTitle: true,
-        elevation: .2,
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Image.network('https://randomuser.me/api/portraits/lego/4.jpg'),
-
-              Text("Hi, I'm Bex, 22"),
-
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                height: 100.0,
-                width: 100.0,
-                decoration: new BoxDecoration(
-                  shape: BoxShape.circle,
-                  border: new Border.all(
-                      color: Color.fromRGBO(200, 200, 200, .5),
-                      width: 12.0,
-                      style: BorderStyle.solid
-                  ),
-                ),
-                child: Icon(
-                  Icons.close,
-                  color: Colors.red,
-                  size: 50.0,
-                ),
-              ),
-              Container(
-                width: 100.0,
-                height: 100.0,
-                decoration: new BoxDecoration(
-                  shape: BoxShape.circle,
-                  border: new Border.all(
-                      color: Color.fromRGBO(200, 200, 200, .5),
-                      width: 12.0,
-                      style: BorderStyle.solid
-                  ),
-                ),
-                child: Icon(
-                  Icons.favorite,
-                  color: Colors.greenAccent,
-                  size: 50.0,
-                ),
-              ),
-            ],
-          )
-        ],
-      ),
-    );
+    return Text('Hello, World');
   }
 }
